d1dac83e6b9dccfcd68e07f4a8d347e5156585ba
akuklinov <akuklinov@outlook.com>
2024-10-09 16:19:04 +0300

    Create product repository.

---
+++ eShop/Category.DAL/Repositories/Contract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.Repositories.Contract
+{
+    public interface IProductRepository
+    {
+        
+    }
+}
---
+++ eShop/Category.DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.DAL.DataContext;
+using Catalog.DAL.Entities;
+using Catalog.DAL.Repositories.Contract;
+using DataAccess.Common;
+using DataAccess.Common.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.Repositories
+{
+    public class ProductRepository : GenericRepository<Product, CatalogDataContext>, IProductRepository
+    {
+        public ProductRepository(CatalogDataContext context):base(context)
+        {
+            
+        }
+    }
+}
