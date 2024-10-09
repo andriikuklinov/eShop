35771da5cb7fc4aaf9f05c9e429326170c447da4
akuklinov <akuklinov@outlook.com>
2024-10-09 19:41:53 +0300

    Implement DateOnly/DateTime converters.

---
+++ eShop/DataAccess.Common/MappingConverters/DateOnlyToDateTimeConverter.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Common.MappingConverters
+{
+    public class DateOnlyToDateTimeConverter : IValueConverter<DateOnly, DateTime>
+    {
+        public DateTime Convert(DateOnly sourceMember, ResolutionContext context)
+        {
+            return sourceMember.ToDateTime(TimeOnly.Parse("00:00 AM"));
+        }
+    }
+}
