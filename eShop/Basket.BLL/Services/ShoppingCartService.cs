6d12cd292503eece74466e5f0ee90da425a422d2
akuklinov <akuklinov@outlook.com>
2024-10-11 18:04:13 +0300

    Implement ShoppingCart service.

---
+++ eShop/Basket.BLL/Services/ShoppingCartService.cs
@@ -0,0 +1,36 @@
+﻿using Basket.BLL.Services.Contract;
+using Basket.DAL.Entities;
+using Basket.DAL.Repositories.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.BLL.Services
+{
+    public class ShoppingCartService:IShoppingCartService
+    {
+        private readonly IShoppingCartRepository _shoppingCartRepository;
+
+        public ShoppingCartService(IShoppingCartRepository shoppingCartRepository)
+        {
+            _shoppingCartRepository = shoppingCartRepository;
+        }
+
+        public async Task DeleteShoppingCart(string userName)
+        {
+            await _shoppingCartRepository.DeleteShoppingCart(userName);
+        }
+
+        public async Task<ShoppingCart?> GetShoppingCart(string userName)
+        {
+            return await _shoppingCartRepository.GetShoppingCart(userName);
+        }
+
+        public async Task<ShoppingCart?> UpdateShoppingCart(ShoppingCart cart)
+        {
+            return await _shoppingCartRepository.UpdateShoppingCart(cart);
+        }
+    }
+}
