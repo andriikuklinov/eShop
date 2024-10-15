56ddbaedb29f3cdf34475e3d53abe0e0849936ad
akuklinov <akuklinov@outlook.com>
2024-10-15 16:46:48 +0300

    Configure ocelot.Development.json file.

--- eShop/eShop.APIGateway/Program.cs
+++ eShop/eShop.APIGateway/Program.cs
@@ -1,12 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddOcelot();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,5 +22,5 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
-
+await app.UseOcelot();
 app.Run();
