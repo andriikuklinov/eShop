68db29e8c470262789009a15c1f00c114ccd7f65
akuklinov <akuklinov@outlook.com>
2024-10-16 17:32:32 +0300

    Implement Order.BLL project.

---
+++ eShop/Order.BLL/Services/Contract/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using Order.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.BLL.Services.Contract
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderDTO>> GetUserOrders(int userId);
+    }
+}
---
+++ eShop/Order.BLL/Services/OrderService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Order.BLL.DTO;
+using Order.BLL.Services.Contract;
+using Order.DAL.Repositories.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.BLL.Services
+{
+    public class OrderService: IOrderService
+    {
+        private readonly IMapper _mapper;
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderService(IMapper mapper, IOrderRepository orderRepository)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+        }
+
+        public async Task<IEnumerable<OrderDTO>> GetUserOrders(int userId)
+        {
+            var orders = await _orderRepository.GetOrders(userId);
+            return _mapper.Map<IEnumerable<OrderDTO>>(orders);
+        }
+    }
+}
