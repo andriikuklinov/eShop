eaaf58f71eddde90a6b2703ef496eeb3192ee6ee
akuklinov <akuklinov@outlook.com>
2024-10-16 16:46:56 +0300

    Implement Order data access project.

---
+++ eShop/Order.DAL/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.DAL.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime OrderTime { get; set; }
+        public int UserId { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ eShop/Order.DAL/Repositories/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Common;
+using DataAccess.Common.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Order.DAL.DataContext;
+using Order.DAL.Entities;
+using Order.DAL.Repositories.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.DAL.Repositories
+{
+    public class OrderRepository : GenericRepository<Order.DAL.Entities.Order, OrderDataContext>, IOrderRepository
+    {
+        public OrderRepository(OrderDataContext context):base(context)
+        {
+            
+        }
+
+        public async Task<IEnumerable<Entities.Order>> GetOrders(int userId)
+        {
+            return await GetAll().Filter($"{{'data':[{{'PropertyName':'Name','Direction':'asc'}}]}}").ToListAsync<Entities.Order>();
+        }
+    }
+}
