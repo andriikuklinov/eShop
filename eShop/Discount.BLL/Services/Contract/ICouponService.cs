21a58d7609209aca56065cf49952cb094602a004
akuklinov <akuklinov@outlook.com>
2024-10-17 18:39:14 +0300

    Implement CouponService.

---
+++ eShop/Discount.BLL/Services/Contract/ICouponService.cs
@@ -0,0 +1,17 @@
+﻿using Discount.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.BLL.Services.Contract
+{
+    internal interface ICouponService
+    {
+        Task<CouponDTO> GetDiscount(int productId);
+        Task<CouponDTO> CreateDiscount(CouponDTO coupon);
+        Task<CouponDTO> UpdateDiscount(CouponDTO coupon);
+        Task<CouponDTO> DeleteDiscount(CouponDTO coupon);
+    }
+}
