a28ca216d7bfb36fda2c7fd9b2181b29709c68e2
akuklinov <akuklinov@outlook.com>
2024-10-17 18:25:14 +0300

    Implement Discount repository.

---
+++ eShop/Discount.DAL/DataContext/DiscountDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Discount.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount.DAL.DataContext
+{
+    public class DiscountDataContext : DbContext
+    {
+        public DiscountDataContext(DbContextOptions<DiscountDataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "tire 24 67 78", DiscountPercentage = 25, Description = "test description Tire" },
+                new Coupon { Id = 1, ProductName = "AGM 26 78", DiscountPercentage = 15, Description = "test description AGM" },
+                new Coupon { Id = 1, ProductName = "AGM 11 66 77", DiscountPercentage = 5, Description = "test description AGM" }
+            );
+        }
+    }
+}
