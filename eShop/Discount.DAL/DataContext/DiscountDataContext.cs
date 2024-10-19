2fe329d0a61f8103787e0f87320d77e7ec21895b
akuklinov <akuklinov@outlook.com>
2024-10-19 17:09:09 +0300

    Fix OnModelCreating method.

--- eShop/Discount.DAL/DataContext/DiscountDataContext.cs
+++ eShop/Discount.DAL/DataContext/DiscountDataContext.cs
@@ -18,9 +18,9 @@ namespace Discount.DAL.DataContext
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Coupon>().HasData(
-                new Coupon { Id = 1, ProductName = "tire 24 67 78", DiscountPercentage = 25, Description = "test description Tire" },
-                new Coupon { Id = 1, ProductName = "AGM 26 78", DiscountPercentage = 15, Description = "test description AGM" },
-                new Coupon { Id = 1, ProductName = "AGM 11 66 77", DiscountPercentage = 5, Description = "test description AGM" }
+                new Coupon { Id = 1, ProductId = 1, DiscountPercentage = 25, Description = "test description Tire" },
+                new Coupon { Id = 1, ProductId = 4, DiscountPercentage = 15, Description = "test description AGM" },
+                new Coupon { Id = 1, ProductId = 5, DiscountPercentage = 5, Description = "test description AGM" }
             );
         }
     }
