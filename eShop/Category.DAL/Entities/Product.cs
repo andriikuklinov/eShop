5df1566570ccba6394e6e57c21d5d4909de20fd6
akuklinov <akuklinov@outlook.com>
2024-10-19 22:26:40 +0300

    AddNewFieldsToProduct migration.

--- eShop/Category.DAL/Entities/Product.cs
+++ eShop/Category.DAL/Entities/Product.cs
@@ -11,10 +11,24 @@ namespace Catalog.DAL.Entities
     {
         public int Id { get; set; }
         [Required]
+        public string Brand { get; set; }
+        [Required]
+        public string Model { get; set; }
+        public string? Season { get; set; }
+        [Required]
         public string Name { get; set; }
         [Required]
         public string Summary { get; set; }
         public string? ImageSrc { get; set; }
+        [Required]
+        public decimal WholesalePrice { get; set; }
+        [Required]
+        public decimal RetailPrice { get; set; }
+        [Required]
+        public int Count { get; set; }
+        public int FreeCount { get; set; }
+        [Required]
+        public string Code { get; set; }
         [Required]
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
