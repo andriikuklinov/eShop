caa54a16750cd694689d6c97f4cb3f4a07af46d2
akuklinov <akuklinov@outlook.com>
2024-10-20 20:52:57 +0300

    Add mapping profiles.

---
+++ eShop/Basket.API/MappingProfiles/ApiMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Basket.API.Models;
+using Basket.BLL.DTO;
+using EventBus.Messages.Common.Events;
+
+namespace Basket.API.MappingProfiles
+{
+    public class ApiMappingProfile: Profile
+    {
+        public ApiMappingProfile()
+        {
+            CreateMap<BasketCheckoutModel, CheckoutDTO>();
+        }
+    }
+}
