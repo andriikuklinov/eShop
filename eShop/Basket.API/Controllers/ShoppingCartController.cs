83b0ec4a1e4e81889a27be465da1fd9bef52d123
akuklinov <akuklinov@outlook.com>
2024-10-20 20:54:29 +0300

    Implement checkout method.

--- eShop/Basket.API/Controllers/ShoppingCartController.cs
+++ eShop/Basket.API/Controllers/ShoppingCartController.cs
@@ -1,5 +1,10 @@
-﻿using Basket.BLL.Services.Contract;
+﻿using AutoMapper;
+using Basket.API.Models;
+using Basket.BLL.DTO;
+using Basket.BLL.Services.Contract;
 using Basket.DAL.Entities;
+using EventBus.Messages.Common.Events;
+using MassTransit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +14,12 @@ namespace Basket.API.Controllers
     [ApiController]
     public class ShoppingCartController : ControllerBase
     {
+        private readonly IMapper _mapper;
         private readonly IShoppingCartService _shoppingCartService;
 
-        public ShoppingCartController(IShoppingCartService shoppingCartService)
+        public ShoppingCartController(IMapper mapper, IShoppingCartService shoppingCartService)
         {
+            _mapper = mapper;
             _shoppingCartService = shoppingCartService;
         }
 
@@ -33,6 +40,13 @@ namespace Basket.API.Controllers
         {
             await _shoppingCartService.DeleteShoppingCart(userName);
             return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Checkout([FromBody] BasketCheckoutModel basketCheckout)
+        {
+            await _shoppingCartService.Checkout(_mapper.Map<CheckoutDTO>(basketCheckout));
+            return Ok("Done");
         }
     }
 }
