c16ccfc5e894e4b8ef35523f98d67cabd3facd62
akuklinov <akuklinov@outlook.com>
2024-10-23 17:31:14 +0300

    CreateProductCommand.

---
+++ eShop/Category.BLL/Commands/CreateProductCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.BLL.Commands
+{
+    internal record CreateProductCommand(int Id, string Brand, string Model, 
+        string Season, string Summary, string? ImgSrc, 
+        decimal WholesalePrice, decimal RetailPrice, int Count, 
+        int FreeCount, string Code, int CategoryId): IRequest<CreateProductResult>;
+
+    internal record CreateProductResult(int Id);
+}
