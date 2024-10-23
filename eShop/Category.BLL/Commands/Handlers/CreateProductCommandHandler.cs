0a8f63bd5516a1a55f807fdde99e6da7bc9f604c
akuklinov <akuklinov@outlook.com>
2024-10-23 17:31:44 +0300

    CreateProductCommandHandler.

---
+++ eShop/Category.BLL/Commands/Handlers/CreateProductCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Catalog.BLL.Commands.Handlers
+{
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
